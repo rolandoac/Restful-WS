0d9b3aadd05e0112568de8b5043815019a7d53fe
rolandoac <rolandoac@gmail.com>
2013-06-06 17:58:28 -0600

    Commit desde el eclipse

--- src/com/ws/ios/servicios/PermisoService.java
+++ src/com/ws/ios/servicios/PermisoService.java
@@ -1,5 +1,36 @@
 package com.ws.ios.servicios;
 
-public class PermisoService {
+import com.ws.ios.dominio.Permiso;
+import com.ws.ios.persistencia.mapeos.*;
+import java.io.*;
+import java.util.ArrayList;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.*;
 
+public class PermisoService {
+	  public ArrayList<Permiso> getPermisos() throws Exception {
+			ArrayList<Permiso> listPermisos = null;
+			
+		    PermisoService permisoService = new PermisoService();
+		    SqlSessionFactory factory = permisoService.getSessionFactory();       
+		    SqlSession session = factory.openSession();
+
+			try {
+				 PermisoMapper mapper = session.getMapper(PermisoMapper.class);
+				 listPermisos = mapper.getPermisos();
+			} 
+			finally {
+	            session.close();
+	        }
+			
+			return listPermisos;
+		}
+
+	    public SqlSessionFactory getSessionFactory() throws IOException {
+	        String res = "com/ws/ios/config/Configuration.xml";
+	        Reader reader = Resources.getResourceAsReader(res);
+	        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+	        
+	        return builder.build(reader);
+	    }
 }
